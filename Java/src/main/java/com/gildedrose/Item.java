9c43bbef9d559200a4b5a71c4982476564db7dad
jolan.stordeur <jolan.stordeur@accenture.com>
2020-11-20 23:19:10 +0100

    [doc] Document Item class a bit to understand what the properties mean

--- Java/src/main/java/com/gildedrose/Item.java
+++ Java/src/main/java/com/gildedrose/Item.java
@@ -5,8 +5,10 @@ public class Item {
 
     public String name;
 
+    //  Denotes the number of days we have to sell the item
     public int sellIn;
 
+    //  Denotes how valuable the item is
     public int quality;
 
     public Item(String name, int sellIn, int quality) {
