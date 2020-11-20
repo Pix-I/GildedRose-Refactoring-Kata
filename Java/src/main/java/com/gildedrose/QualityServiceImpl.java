bc183179509c4c20062163cf0b3501be5cd3f114
jolan.stordeur <jolan.stordeur@accenture.com>
2020-11-20 23:51:29 +0100

    [cleanup] Move check to see if quality can be upgrade/downgraded to the upgrade/downgrade method

--- Java/src/main/java/com/gildedrose/QualityServiceImpl.java
+++ Java/src/main/java/com/gildedrose/QualityServiceImpl.java
@@ -5,47 +5,45 @@ public class QualityServiceImpl implements QualityService {
     public void updateQualityOfItem(Item item) {
         if (isAgedBrieTicket(item)
                 || isBackstageTicket(item)) {
-            if (item.quality < 50) {
-                upgradeQuality(item);
-
-                if (isBackstageTicket(item)) {
-                    if (item.sellIn < 11) {
-                        if (item.quality < 50) {
-                            upgradeQuality(item);
-                        }
-                    }
-
-                    if (item.sellIn < 6) {
-                        if (item.quality < 50) {
-                            upgradeQuality(item);
-                        }
-                    }
-                }
-            }
+            upgradeQualityIfNeeded(item);
         } else {
-            if (item.quality > 0) {
-                if (isNotALegendary(item)) {
-                    downGradeQuality(item);
-                }
+            if (isNotALegendary(item)) {
+                downGradeQuality(item);
             }
         }
-
         if (isNotALegendary(item)) {
             item.sellIn = item.sellIn - 1;
         }
-
         if (item.sellIn < 0) {
-            if (isAgedBrieTicket(item)) {
-                if (item.quality < 50) {
+            updateQualityOfOverdueItem(item);
+        }
+    }
+
+    private void upgradeQualityIfNeeded(Item item) {
+        if (item.quality < 50) {
+            upgradeQuality(item);
+
+            if (isBackstageTicket(item)) {
+                if (item.sellIn < 11) {
                     upgradeQuality(item);
                 }
+
+                if (item.sellIn < 6) {
+                    upgradeQuality(item);
+                }
+            }
+        }
+    }
+
+    private void updateQualityOfOverdueItem(Item item) {
+        if (isAgedBrieTicket(item)) {
+            upgradeQuality(item);
+        } else {
+            if (isBackstageTicket(item)) {
+                item.quality = 0;
             } else {
-                if (isBackstageTicket(item)) {
-                    item.quality = 0;
-                } else {
-                    if (item.quality > 0 && isNotALegendary(item)) {
-                        downGradeQuality(item);
-                    }
+                if (isNotALegendary(item)) {
+                    downGradeQuality(item);
                 }
             }
         }
@@ -64,10 +62,14 @@ public class QualityServiceImpl implements QualityService {
     }
 
     private void upgradeQuality(Item item) {
-        item.quality = item.quality + 1;
+        if (item.quality < 50) {
+            item.quality++;
+        }
     }
 
     private void downGradeQuality(Item item) {
-        item.quality = item.quality - 1;
+        if (item.quality > 0) {
+            item.quality--;
+        }
     }
 }
