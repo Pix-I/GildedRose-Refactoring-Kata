5fb897a4cf363f205b59fdd34b305ad8adb897ee
jolan.stordeur <jolan.stordeur@accenture.com>
2020-11-20 23:39:00 +0100

    [testing] Add some test cases just in case (Ages brie doesn't make sense)

---
+++ Java/src/test/java/com/gildedrose/QualityServiceImplTest.java
@@ -0,0 +1,59 @@
+package com.gildedrose;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class QualityServiceImplTest {
+
+    private QualityService qualityService = new QualityServiceImpl();
+
+    @Test
+    void updateQualityOfItem_neverNegative() {
+        Item item = new Item("foo", 0, 0);
+        qualityService.updateQualityOfItem(item);
+        assertEquals(0,item.quality);
+    }
+
+    @Test
+    void updateQualityOfItem_normalTempo() {
+        Item item = new Item("foo", 10, 10);
+        qualityService.updateQualityOfItem(item);
+        assertEquals(9,item.quality);
+    }
+
+    @Test
+    void updateQualityOfItem_pastDueDateTempo() {
+        Item item = new Item("foo", 0, 10);
+        qualityService.updateQualityOfItem(item);
+        assertEquals(8,item.quality);
+    }
+
+    // Does it make sense for agedBrie to get quality twice as fast once it's past the sellIn date?
+    @Test
+    void updateQualityOfItem_agedBrie() {
+        Item item = new Item("Aged Brie", 0, 10);
+        qualityService.updateQualityOfItem(item);
+        assertEquals(12,item.quality);
+    }
+
+    @Test
+    void updateQualityOfItem_agedBrie_normal() {
+        Item item = new Item("Aged Brie", 10, 10);
+        qualityService.updateQualityOfItem(item);
+        assertEquals(11,item.quality);
+    }
+
+    @Test
+    void updateQualityOfItem_agedBrie_maxedOut() {
+        Item item = new Item("Aged Brie", 0, 49);
+        qualityService.updateQualityOfItem(item);
+        assertEquals(50,item.quality);
+    }
+    @Test
+    void updateQualityOfItem_sulfuras() {
+        Item item = new Item("Sulfuras, Hand of Ragnaros", 0, 80);
+        qualityService.updateQualityOfItem(item);
+        assertEquals(80,item.quality);
+    }
+}
