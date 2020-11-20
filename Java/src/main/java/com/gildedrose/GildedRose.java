ef8098e4fdd294b5cfd5baab5a041eea44d99089
jolan.stordeur <jolan.stordeur@accenture.com>
2020-11-20 23:26:03 +0100

    [refactor] Add a QualityService interface/class to make testing easier

--- Java/src/main/java/com/gildedrose/GildedRose.java
+++ Java/src/main/java/com/gildedrose/GildedRose.java
@@ -3,6 +3,9 @@ package com.gildedrose;
 class GildedRose {
     private final Item[] items;
 
+    private final QualityService qualityService = new QualityServiceImpl();
+
+
     public GildedRose(Item[] items) {
         this.items = items;
     }
@@ -13,66 +16,8 @@ class GildedRose {
 
     public void updateQuality() {
         for (Item item : items) {
-            updateQualityOfItem(item);
+            qualityService.updateQualityOfItem(item);
         }
     }
 
-    private void updateQualityOfItem(Item item) {
-        if (!item.name.equals("Aged Brie")
-                && !item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-            if (item.quality > 0) {
-                if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
-                    downGradeQuality(item);
-                }
-            }
-        } else {
-            if (item.quality < 50) {
-                upgradeQuality(item);
-
-                if (item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                    if (item.sellIn < 11) {
-                        if (item.quality < 50) {
-                            upgradeQuality(item);
-                        }
-                    }
-
-                    if (item.sellIn < 6) {
-                        if (item.quality < 50) {
-                            upgradeQuality(item);
-                        }
-                    }
-                }
-            }
-        }
-
-        if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
-            item.sellIn = item.sellIn - 1;
-        }
-
-        if (item.sellIn < 0) {
-            if (!item.name.equals("Aged Brie")) {
-                if (!item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                    if (item.quality > 0) {
-                        if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
-                            downGradeQuality(item);
-                        }
-                    }
-                } else {
-                    item.quality = item.quality - item.quality;
-                }
-            } else {
-                if (item.quality < 50) {
-                    upgradeQuality(item);
-                }
-            }
-        }
-    }
-
-    private void upgradeQuality(Item item) {
-        item.quality = item.quality + 1;
-    }
-
-    private void downGradeQuality(Item item) {
-        item.quality = item.quality - 1;
-    }
 }
---
+++ Java/src/main/java/com/gildedrose/QualityService.java
@@ -0,0 +1,5 @@
+package com.gildedrose;
+
+public interface QualityService {
+    void updateQualityOfItem(Item item);
+}
