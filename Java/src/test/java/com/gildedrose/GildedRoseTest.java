d4c3de9cc25abdfc0313bd15dc70b7f1b7a8abab
jolan.stordeur <jolan.stordeur@accenture.com>
2020-11-20 23:06:00 +0100

    [cleanup] Fix failing test and use proper accessor

--- Java/src/test/java/com/gildedrose/GildedRoseTest.java
+++ Java/src/test/java/com/gildedrose/GildedRoseTest.java
@@ -7,11 +7,12 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 class GildedRoseTest {
 
     @Test
-    void foo() {
-        Item[] items = new Item[] { new Item("foo", 0, 0) };
+    void testSomething() {
+        Item singleItem = new Item("foo", 0, 0);
+        Item[] items = new Item[] {singleItem};
         GildedRose app = new GildedRose(items);
         app.updateQuality();
-        assertEquals("fixme", app.items[0].name);
+        assertEquals("foo", app.getItems()[0].name);
     }
 
 }
