f6352db6db372fe265aee12156f45a7f2aebfb1b
jolan.stordeur <jolan.stordeur@accenture.com>
2020-11-20 23:41:15 +0100

    [cleanup] Make if statements easier to read

--- Java/src/main/java/com/gildedrose/QualityServiceImpl.java
+++ Java/src/main/java/com/gildedrose/QualityServiceImpl.java
@@ -3,54 +3,62 @@ package com.gildedrose;
 public class QualityServiceImpl implements QualityService {
 
     public void updateQualityOfItem(Item item) {
-        if (!item.name.equals("Aged Brie")
-                && !item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+        if (isABackstageTicket(item)
+                || item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+                    if (item.quality < 50) {
+                        upgradeQuality(item);
+
+                        if (item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+                            if (item.sellIn < 11) {
+                                if (item.quality < 50) {
+                                    upgradeQuality(item);
+                                }
+                            }
+
+                            if (item.sellIn < 6) {
+                                if (item.quality < 50) {
+                                    upgradeQuality(item);
+                                }
+                            }
+                        }
+                    }
+                } else {
             if (item.quality > 0) {
-                if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
+                if (isNotALegendary(item)) {
                     downGradeQuality(item);
-                }
-            }
-        } else {
-            if (item.quality < 50) {
-                upgradeQuality(item);
-
-                if (item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                    if (item.sellIn < 11) {
-                        if (item.quality < 50) {
-                            upgradeQuality(item);
-                        }
-                    }
-
-                    if (item.sellIn < 6) {
-                        if (item.quality < 50) {
-                            upgradeQuality(item);
-                        }
-                    }
                 }
             }
         }
 
-        if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
+        if (isNotALegendary(item)) {
             item.sellIn = item.sellIn - 1;
         }
 
         if (item.sellIn < 0) {
-            if (!item.name.equals("Aged Brie")) {
+            if (isABackstageTicket(item)) {
+                if (item.quality < 50) {
+                    upgradeQuality(item);
+                }
+            } else {
                 if (!item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
                     if (item.quality > 0) {
-                        if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
+                        if (isNotALegendary(item)) {
                             downGradeQuality(item);
                         }
                     }
                 } else {
                     item.quality = item.quality - item.quality;
                 }
-            } else {
-                if (item.quality < 50) {
-                    upgradeQuality(item);
-                }
             }
         }
+    }
+
+    private boolean isABackstageTicket(Item item) {
+        return item.name.equals("Aged Brie");
+    }
+
+    private boolean isNotALegendary(Item item) {
+        return !item.name.equals("Sulfuras, Hand of Ragnaros");
     }
 
     private void upgradeQuality(Item item) {
